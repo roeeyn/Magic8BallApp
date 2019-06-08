6d3568e5b73c8529c08e1bef440db6d1172ac1ef
Rodrigo M <rodrigo.medina.neri@gmail.com>
2019-06-07 21:13:57 -0500

    Initial commit. I added the assets and the logic to generate a random image when clicked or shared

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -9,12 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var outputBallMessage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    func updateRandomBall(){
+        let images = ["ball1", "ball2", "ball3", "ball4", "ball5"]
+        let randomImageIndex = Int(arc4random_uniform(5))
+        outputBallMessage.image = UIImage(named: images[randomImageIndex])
+    }
+    
+    @IBAction func onAskClicked(_ sender: Any) {
+        updateRandomBall()
+    }
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        updateRandomBall()
+    }
+    
 }
 
